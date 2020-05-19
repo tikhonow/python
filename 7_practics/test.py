48487e8f4f310cdac00c275e8ed93b83f6f86035
Ruslan Tikhonov <tihoo99@icloud.com>
2020-05-19 19:02:11 +1000

    Add: task 1

---
+++ 7_practics/test.py
@@ -0,0 +1,33 @@
+import sys  # sys нужен для передачи argv в QApplication
+from PyQt5 import QtWidgets
+from PyQt5 import QtPrintSupport
+
+#--------
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+#=--------
+import design  #конвертированный файл дизайна
+import os
+class HtmlEditor(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.action_4.triggered.connect(self.close_application)#инициализируем кнопку выхода из программы
+
+    def close_application(self):
+        sys.exit()
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = HtmlEditor()  # Создаём объект класса HtmlEditor
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
+
+
+def exit_from_editor(self):
+    self.action_4 = sys.exit()
