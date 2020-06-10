495f1119be8e9d2c2618c044d0ee97f2d0c27e17
Ruslan Tikhonov <tihoo99@icloud.com>
2020-06-10 13:24:28 +1000

    task "BC" is done

---
+++ 8_practics/B/desb.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'desb.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(512, 554)
+        MainWindow.setContextMenuPolicy(QtCore.Qt.PreventContextMenu)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.combo = QtWidgets.QComboBox(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.combo.sizePolicy().hasHeightForWidth())
+        self.combo.setSizePolicy(sizePolicy)
+        self.combo.setMaximumSize(QtCore.QSize(16777215, 90))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.combo.setFont(font)
+        self.combo.setObjectName("combo")
+        self.combo.addItem("")
+        self.verticalLayout.addWidget(self.combo)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setMaximumSize(QtCore.QSize(16777215, 90))
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setMaximumSize(QtCore.QSize(16777215, 90))
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout.addWidget(self.label_2)
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setMaximumSize(QtCore.QSize(16777215, 90))
+        self.textEdit.setObjectName("textEdit")
+        self.verticalLayout.addWidget(self.textEdit)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.commandLinkButton = QtWidgets.QCommandLinkButton(self.centralwidget)
+        self.commandLinkButton.setMaximumSize(QtCore.QSize(16777215, 90))
+        self.commandLinkButton.setCursor(QtGui.QCursor(QtCore.Qt.OpenHandCursor))
+        self.commandLinkButton.setObjectName("commandLinkButton")
+        self.horizontalLayout.addWidget(self.commandLinkButton)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.verticalLayout_2.addWidget(self.tableWidget)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 512, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Залание B&C"))
+        self.combo.setItemText(0, _translate("MainWindow", "Не выбрано"))
+        self.label.setText(_translate("MainWindow", "Вы можете выбрать из выпадающего списка выше "))
+        self.label_2.setText(_translate("MainWindow", "или ввести трубемый жанр самостоятельно"))
+        self.commandLinkButton.setText(_translate("MainWindow", "ПОИСК"))
